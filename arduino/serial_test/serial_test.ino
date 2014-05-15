4447575c81d83eeef278f42a780c608f775b2480
Woody Zantzinger <wz2ae@virginia.edu>
2014-05-15 13:18:52 -0400

    Updates

--- arduino/serial_test/serial_test.ino
+++ arduino/serial_test/serial_test.ino
@@ -1,13 +1,66 @@
+
+#include <Wire.h>
+
+#define ACCELADDR 0x53
+
+union XYZBuffer{
+  struct{
+    short x,y,z;
+  } value;
+  byte buff[6];
+};
+
+void init_accel(int device_id) {
+  Serial.print("We got here!");
+  Wire.beginTransmission(device_id);
+  Wire.write(0x00);
+  Wire.endTransmission();
+  Wire.requestFrom(device_id, 1);
+  byte ch;
+  Serial.print("We got here!");
+  while (!Wire.available()) {
+    ch = Wire.read();
+  }
+  Serial.print("ACCEL INIT IS 0x");
+  Serial.println(ch,HEX);
+  Wire.beginTransmission(device_id);
+  Wire.write(0x2d);
+  Wire.write(0x08);
+  Wire.endTransmission();
+  Wire.beginTransmission(device_id);
+  Wire.write(0x38);
+  Wire.write(0x84);
+  Wire.endTransmission();
+}
+
 void setup() {
   Serial.begin(9600); // set the baud rate
+  Serial.println("Ready!"); // print "Ready" once
+  Serial.print("Ready"); // print "Ready" once
   Serial.println("Ready"); // print "Ready" once
+  Serial.print("Ready"); // print "Ready" once
+  init_accel(ACCELADDR);
 }
 
 void loop() {
-  char inByte = ' ';
-  if(Serial.available()){ // only send data back if data has been sent
-    char inByte = Serial.read(); // read the incoming data
-    Serial.println(inByte); // send the data back in a new line so that it is not all one long line
+  while (true) {
+    XYZBuffer temp;
+    Wire.beginTransmission(ACCELADDR);
+    Wire.write(0x32);
+    Wire.endTransmission();
+    Wire.requestFrom(ACCELADDR, 6);
+    long start = millis();
+    while(Wire.available()) {
+      for (int i=1; i< 6; i++) {
+        temp.buff[i] = Wire.read();
+      }
+    }
+    Serial.print(temp.value.x);
+    Serial.print(':');
+    Serial.print(temp.value.y);
+    Serial.print(':');
+    Serial.print(temp.value.z);
+    Serial.print('\n');
+    delay(100); // delay for 1/10 of a second
   }
-  delay(100); // delay for 1/10 of a second
 }
--- python/test_com.py
+++ python/test_com.py
@@ -36,22 +36,10 @@ def main():
 #Loop in reading then sending messages
 
     while True:
-        cmd = input('Enter Command: ')
-        try:
-            print "Sending Message:",
-            ser.write(cmd) #
-        except serial.SerialTimeoutException as e:
-            print print_msg.FAIL
-            print e.errno
-            print e.filename
-            print e.strerror
-            sys.exit(0)
-        else:
-            print print_msg.OK
-
-        byteData = ser.read(8)
-        print byteData
-
+        byteData = ser.read(50)
+        if len(byteData) > 0:
+            print byteData
+        sleep(.1)
     ser.close()
 
 if __name__ == "__main__":
